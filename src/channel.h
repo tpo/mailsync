7b7812601e4b698689e570a7696bec5bf3d44a03
Tomas Pospisek <tpo_hp@sourcepole.ch>
2013-01-12 00:05:55 +0100

    trying to make the class declaration more readable

--- src/channel.h
+++ src/channel.h
@@ -15,32 +15,34 @@ class Channel
 //
 //////////////////////////////////////////////////////////////////////////
 {
-  public:
-    string name;
-    Store store_a;
-    Store store_b;
-    string msinfo;
-    Passwd passwd;
-    unsigned long sizelimit;
-
+  public: // constructor
     Channel(): name(), msinfo(), passwd(), sizelimit(0) {};
 
+  public: // methods
     void print(FILE* f);
 
-    void set_passwd(string password) { passwd.set_passwd(password);}
-    void set_sizelimit(const string& sizelim)
-    {
-      sizelimit=strtoul(sizelim.c_str(),NULL,10);
-    }
-    bool read_seen_last_time( MsgIdsPerMailbox& mids_per_box, 
-                              MailboxMap& deleted_mailboxes);
-    bool open_for_copying( string mailbox_name, enum direction_t direction);
-    bool copy_message( unsigned long msgno,
-                       const MsgId& msgid,
-                       string mailbox_name,
-                       enum direction_t direction);
-    bool write_thistime_seen( const MailboxMap& deleted_mailboxes,
-                                    MsgIdsPerMailbox& thistime);
+    void set_passwd    (      string  password) {  passwd.set_passwd(password);                }
+    void set_sizelimit (const string& sizelim)  {  sizelimit=strtoul(sizelim.c_str(),NULL,10); }
+
+    bool read_seen_last_time (       MsgIdsPerMailbox&  mids_per_box, 
+                                     MailboxMap&        deleted_mailboxes);
+    bool write_thistime_seen ( const MailboxMap&        deleted_mailboxes,
+                                     MsgIdsPerMailbox&  thistime);
+
+    bool open_for_copying ( string             mailbox_name,
+		            enum direction_t   direction);
+    bool copy_message     ( unsigned long      msgno,
+                            const MsgId&       msgid,
+                            string             mailbox_name,
+                            enum direction_t   direction);
+
+  public: // attributes
+    string         name;
+    Store          store_a;
+    Store          store_b;
+    string         msinfo;
+    Passwd         passwd;
+    unsigned long  sizelimit;
 };
 
 #define __MAILSYNC_CHANNEL__

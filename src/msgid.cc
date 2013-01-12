aee3b7e6b88664c1b864cc874767728ea217b796
Tomas Pospisek <tpo_hp@sourcepole.ch>
2013-01-12 11:31:37 +0100

    improve readability

--- src/msgid.cc
+++ src/msgid.cc
@@ -19,21 +19,21 @@
 extern options_t options;
 
 #ifdef HAVE_MD5
- #define MD5BLKLEN 64           /* MD5 block length */
- #define MD5DIGLEN 16           /* MD5 digest length */
+ #define MD5BLKLEN 64             /* MD5 block length */
+ #define MD5DIGLEN 16             /* MD5 digest length */
 
  typedef struct {
-   unsigned long chigh;         /* high 32bits of byte count */
-   unsigned long clow;          /* low 32bits of byte count */
-   unsigned long state[4];      /* state (ABCD) */
-   unsigned char buf[MD5BLKLEN];        /* input buffer */
-   unsigned char *ptr;          /* buffer position */
+   unsigned long chigh;           /* high 32bits of byte count */
+   unsigned long clow;            /* low 32bits of byte count */
+   unsigned long state[4];        /* state (ABCD) */
+   unsigned char buf[MD5BLKLEN];  /* input buffer */
+   unsigned char *ptr;            /* buffer position */
  } MD5CONTEXT;
 
  extern "C" {
-  void md5_init (MD5CONTEXT *ctx);
-  void md5_update (MD5CONTEXT *ctx,unsigned char *data,unsigned long len);
-  void md5_final (unsigned char *digest,MD5CONTEXT *ctx);
+  void md5_init   (MD5CONTEXT*    ctx );
+  void md5_update (MD5CONTEXT*    ctx,    unsigned char* data, unsigned long len);
+  void md5_final  (unsigned char* digest, MD5CONTEXT*    ctx );
  }
 #endif // HAVE_MD5
 

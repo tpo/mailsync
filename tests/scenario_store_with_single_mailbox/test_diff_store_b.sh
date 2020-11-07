b59b68135899b3013574e8010fe24bb052d6e068
Tomas Pospisek <tpo_hp@sourcepole.ch>
2020-11-07 22:16:49 +0100

    add tests for store diffs

---
+++ tests/scenario_store_with_single_mailbox/test_diff_store_b.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# from where this script is ...
+source "$( dirname "$( realpath $0 )" )"/../lib/lib.sh
+
+pre_run
+
+# sync store_a and store_b
+run_mailsync -vw -vp -m channel_store_a_store_b store_b
+
+post_run
